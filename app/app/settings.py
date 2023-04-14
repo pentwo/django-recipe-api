f11dba81e67b324c7ded87b1efbf78f465086404
Steven Peng <pentwo@gmail.com>
2023-04-14 14:04:00 +0800

    FIX: revert SECRET_KEY in settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = "django-insecure-8hxd-(yw9)$@$ip&na!)un!ou=wlg5)*-m)!)ij(tirrpy#2sh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
