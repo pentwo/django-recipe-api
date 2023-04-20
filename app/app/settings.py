5e8796d2f57b15373cf7169c6f6f3fa04cadca88
Steven Peng <pentwo@gmail.com>
2023-04-20 18:00:51 +0800

    DEVOP: Setting up proxy and dockerfile for deploy

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8hxd-(yw9)$@$ip&na!)un!ou=wlg5)*-m)!)ij(tirrpy#2sh"
+SECRET_KEY = os.environ.get("SECRET_KEY", "secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get("ALLOWED_HOSTS", "").split(","),
+    )
+)
+
 
 #              python manage.py migrate &&
 
---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# This script is used to run the application in production mode.
+set -e
+
+python manage.py wait_for_db
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
